f718c31f3a2e025f019bdb84b0c466eae244f48b
gianluca-capelo <gianluca.capelo@despegar.com>
2025-12-19 12:25:06 -0300

    pipeline

--- src/model/process_data/process_data.py
+++ src/model/process_data/process_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from src.hand_analysis.loader.load_last_split import load_last_analysis
+from src.loader.load_last_split import load_last_analysis
 
 #No hay eval set, solo train set
 df, _= load_last_analysis()
