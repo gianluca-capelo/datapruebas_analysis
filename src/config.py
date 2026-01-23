54ee3829efa31c12d4e2e5dbf51d13b805d32b5b
gianluca-capelo <gianluca.capelo@despegar.com>
2026-01-23 08:58:56 -0300

    refactor

--- src/config.py
+++ src/config.py
@@ -56,7 +56,7 @@ CONSECUTIVE_POINTS = 5
 CORRECT_THRESHOLD = 10
 CUT_CRITERIA = "MINIMUM_TARGETS"
 CALCULATE_CROSSES = False
-INTERPOLATE_TRAJECTORY = False
+INTERPOLATE_TRAJECTORY = True
 
 # =============================================================================
 # ML Pipeline Configuration
