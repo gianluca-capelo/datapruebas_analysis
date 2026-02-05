d519c6a3fdf30b7159ffd03c5bb2c4ff4b229ea0
gianluca-capelo <gianluca.capelo@despegar.com>
2026-02-05 09:31:09 -0300

    refactor

--- src/config.py
+++ src/config.py
@@ -64,7 +64,7 @@ TARGET_RADIUS_MULTIPLIER = 1.15  # Multiplier for target radius (1.0 = no change
 
 import xgboost as xgb
 from sklearn.dummy import DummyRegressor
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor  # , RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet, LogisticRegression
 from sklearn.svm import SVR, SVC
 
